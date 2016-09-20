867ae5251c4350d9159ddb774dd2d578b6f48fca
Michael Vargas <mvargas@csumb.edu>
2016-09-20 18:46:04 +0000

    added displayWinners() function

--- review2.php
+++ review2.php
@@ -24,6 +24,39 @@ function displayRandomCard() {
 
 
 
+function displayWinners() {
+    
+    $player_name = array("mike", "scot", "Tris", "suzy");
+    $player_score = array(35, 42, 43, 38);
+    $winner = array();
+    
+    for ($i = 0; $i < 4; $i++) //number of players = 4
+    {
+        if ($player_score[$i] <= 42) //under 42
+        {
+            if ($player_score[$i] == 42) // exactly 42
+            {
+                $winner[$i] = 8; //keeps track of which player in array won
+                
+            }
+            else $winner[$i] = 5; //puts random number to disregard player
+        }
+        else $winner[$i] = 7; //excludes the player that went over
+    }
+    
+    
+    print_r($winner);
+//   print($player_score[1]);
+
+    while ($x < 4)
+    {
+        if ($winner[$x] == 8)
+        echo $player_name[$x] . "won. </br>";
+        $x++;
+    }
+
+
+}
 
 
 ?>
@@ -38,6 +71,7 @@ function displayRandomCard() {
     <body>
         
         <?=displayRandomCard() ?>
+        <?=displayWinners() ?>
     
 
     </body>
