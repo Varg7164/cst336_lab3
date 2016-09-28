773b221612df9bdf433e8e235adff8edfc8c585d
Michael Vargas <mvargas@csumb.edu>
2016-09-28 06:04:31 +0000

    added more code in displayWinners()

--- review2.php
+++ review2.php
@@ -55,31 +55,36 @@ function displayHand (){
 function displayWinners() {
     
     $player_name = array("mike", "scot", "Tris", "suzy");
-    $player_score = array(35, 42, 43, 38);
+    $player_score = array(31, 45, 42, 39);
     $winner = array();
+    $totalScore = 0;
     
     for ($i = 0; $i < 4; $i++) //number of players = 4
     {
         if ($player_score[$i] <= 42) //under 42
         {
             if ($player_score[$i] == 42) // exactly 42
-            {
                 $winner[$i] = 8; //keeps track of which player in array won
-                
+ 
+            else {
+                $winner[$i] = 5; //puts random number to disregard player
+                $totalScore += $player_score[$i];
             }
-            else $winner[$i] = 5; //puts random number to disregard player
         }
-        else $winner[$i] = 7; //excludes the player that went over
+        else {
+            $winner[$i] = 7; //excludes the player that went over
+            $totalScore += $player_score[$i];
+        }
     }
     
     
     print_r($winner);
 //   print($player_score[1]);
 
-    while ($x < 4)
+    while ($x < 4) //displays the winner and the total score
     {
         if ($winner[$x] == 8)
-        echo $player_name[$x] . "won. </br>";
+            echo $player_name[$x]. " won ". $totalScore . "<br/>";
         $x++;
     }
 
